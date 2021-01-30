4cb8e12903fbd5cf78a591fac315ed72258d2ba1
Grant Gillespie <grantgi2019@gmail.com>
2021-01-30 16:07:55 -0500

    Numbers increase when pushing 'd'

--- thisisthelasttry/src/components/character.jsx
+++ thisisthelasttry/src/components/character.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import useKeyPress from './keyPress'
 import sprite_running_1 from '../assets/png/Run (1).png'
 import sprite_running_2 from '../assets/png/Run (2).png'
 import sprite_running_3 from '../assets/png/Run (3).png'
@@ -8,17 +9,19 @@ import sprite_running_6 from '../assets/png/Run (6).png'
 import sprite_running_7 from '../assets/png/Run (7).png'
 import sprite_running_8 from '../assets/png/Run (8).png'
 
-import {keyHandler, KEYPRESS} from 'react-key-handler'
-
 function Character(){
 
     const [state, setState] = useState (() => {
         return "Something random"
     })
 
+    const running = useKeyPress('d')
+
     return (
         <div>
-            <img src={sprite_running_1} alt="Yeah image not there... buddy" />
+            {running && setTimeout(() => {
+                <div>Running</div>
+            }, 1000)}
         </div>
     )
 }
--- thisisthelasttry/src/components/keyPress.jsx
+++ thisisthelasttry/src/components/keyPress.jsx
@@ -1,17 +1,35 @@
-import React, { useState } from 'react'
-import {keyHandler, KEYPRESS} from 'react-key-handler'
+import React, { useState, useEffect } from 'react'
 
-function handleKeyPress({keyValue}){
-    return(
-        <div>
-            {keyValue === 'd' && (
-                <ol>
-                    <li>hello</li>
-                    <li>world</li>
-                </ol>
-            )}
-        </div>
-    );
-}
+function useKeyPress(targetKey) {
+    // State for keeping track of whether key is pressed
+    const [keyPressed, setKeyPressed] = useState(false);
+  
+    // If pressed key is our target key then set to true
+    function downHandler({ key }) {
+      if (key === targetKey) {
+        setKeyPressed(true);
+      }
+    }
+  
+    // If released key is our target key then set to false
+    const upHandler = ({ key }) => {
+      if (key === targetKey) {
+        setKeyPressed(false);
+      }
+    };
+  
+    // Add event listeners
+    useEffect(() => {
+      window.addEventListener('keydown', downHandler);
+      window.addEventListener('keyup', upHandler);
+      // Remove event listeners on cleanup
+      return () => {
+        window.removeEventListener('keydown', downHandler);
+        window.removeEventListener('keyup', upHandler);
+      };
+    }, []); // Empty array ensures that effect is only run on mount and unmount
+  
+    return keyPressed;
+  }
 
-export default keyHandler({keyEventName: KEYPRESS, keyValue: 'd'})(handleKeyPress);
+export default useKeyPress
